7b2392b1a5efdc3a6026714f29b43a652044914e
japnit01 <japnit2012@gmail.com>
2021-05-16 00:44:56 +0530

    encoding

--- Book_Management.cpp
+++ Book_Management.cpp
@@ -1,17 +1,194 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+map<char,string> codes;
+string compressed = "";
+
+struct minheapnode{
+    char l;
+    int freq;
+    minheapnode *left,*right;
+
+    minheapnode(char l,int freq)
+    {
+        left = right = NULL;
+        this->l = l;
+        this->freq = freq;
+    }
+};
+
+struct cmp{
+    bool operator()(minheapnode* a,minheapnode* b)
+    {
+        return a->freq>b->freq;
+    }
+};
+
+void text_encode(string text)
+{
+    for(int i=0;i<text.size();i++)
+    {
+        compressed+=codes[text[i]];
+    }
+}
+
+void codestable(minheapnode* root,string str)
+{
+    if(!root)
+        return ;
+
+    if(root->l != '#')
+    {
+        //cout<<root->l<<": "<<str<<"\n";
+        codes[root->l] = str;
+    }
+
+    codestable(root->left,str + "0");
+    codestable(root->right,str + "1");
+}
+
+void preorder(minheapnode* root,string str,string a)
+{
+    if(!root)
+    {
+        return ;
+    }
+
+    compressed+=a;
+    if(root->l != '#')
+    {
+        compressed+=root->l;
+    }
+
+    preorder(root->left,str,"0");
+    preorder(root->right,str,"1");
+}
+
+void huffman(map<char,int> freq)
+{
+     
+    priority_queue<minheapnode*,vector<minheapnode*>,cmp> q;
+
+    for(auto it = freq.begin();it!=freq.end();it++)
+    {
+        q.push(new minheapnode(it->first,it->second));
+    }
+
+    //printq(q);
+    minheapnode *top,*right,*left;
+
+    while(q.size()!=1)
+    {
+        left  = q.top();
+        q.pop();
+
+        right = q.top();
+        q.pop();
+
+        top = new minheapnode('#',left->freq + right->freq);
+
+        top->left = left;
+        top->right = right;
+        
+        q.push(top);
+    }
+
+   codestable(q.top(),"");
+   string a;
+   preorder(q.top(),"",a);
+}
+
+void calcfreq(string text)
+{
+    map<char,int> freq;
+    for(int i=0;i<text.size();i++)
+    {
+        freq[text[i]]++;
+    }
+    /*
+    for(auto it = freq.begin();it!=freq.end();++it)
+    {
+        cout<<it->first<<" "<<it->second<<"\n";
+    }
+    
+    cout<<"\n";*/
+    huffman(freq);
+}
+
 void downloadfile(){
     cout<<"\nDownload book\n";
 }
 
 void addfile(){
     cout<<"\nAdd Book\n";
-}
+    string name,author,subject,tag,file;
+    cout<<"Name: \n";
+    cin>>name;
+    cout<<"Author: \n";
+    cin>>author;
+    cout<<"Subject: \n";
+    cin>>subject;
+    cout<<"Tag (Class/Engineering/Medical): \n";
+    cin>>tag;
 
-void orderstatus()
-{
-    cout<<"\nOrder Status\n";
+    string inputfile,outputfile;
+
+    cout<<"Enter the name of file: \n";
+    cin>>inputfile;
+
+    ifstream inpfile;
+    string s="",temp;
+
+    inpfile.open("files/" + inputfile,ios::in);
+    if(!inpfile)
+    {
+        cout<<"File not found\n";
+    }
+    else
+    {
+        //inpfile>>s;
+        while(!inpfile.eof())
+        {
+            getline(inpfile,temp);
+            //cout<<temp<<"\n";
+            s+=temp;
+        }
+        inpfile.close();
+        calcfreq(s);
+      
+        int n = compressed.size();
+        //cout<<n<<"\n";
+        string tree_size="";
+
+        for (int i = 31; i >= 0; i--) {
+            int k = n >> i;
+            if (k & 1)
+            tree_size+="1";
+            else
+            tree_size+="0";
+        }
+        //cout<<tree_size.size()<<"\n";
+        compressed = tree_size + compressed;
+        //cout<<s<<"\n";
+        text_encode(s);
+        //cout<<compressed.size()<<"\n";
+    
+        ofstream outpfile;
+
+        outputfile = inputfile.substr(0,inputfile.size()-4) + "_compressed.txt";
+        outpfile.open("compressed_files/" + outputfile,ios::out);
+        
+        if(!outpfile)
+        {
+            cout<<"File not created\n";
+        }
+        else
+        {
+            //cout<<compressed.size()<<"\n";
+            outpfile<<compressed<<endl;
+        }
+        outpfile.close();
+    }
 }
 
 void cart()
@@ -36,6 +213,11 @@ void bookbank()
     {
         downloadfile();
     }
+}
+
+void orderstatus()
+{
+    cout<<"\nOrder Status\n";
 }
 
 void orderbook()
